2680d800bfeb0968968e141628160bdaca5ebe3f
TayloredSuites <ibnnureevsoraya@gmail.com>
2022-09-22 02:53:14 -0700

    Added 64-bit char sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,11 @@ int main(void)
 	printf("Size of a long long int: %lli byte(s)", sizeof(long long int));
 	printf("Size of a float: %f byte(s)", sizeof(float));
 
+        printf("Size of a char: %c byte(s)", sizeof(char));                                                                                                     
+        printf("Size of an int: %i byte(s)", sizeof(int));        
+	printf("Size of a long int: %li byte(s)", sizeof(long int));                                                                            
+	printf("Size of a long long int: %lli byte(s)", sizeof(long long int));                                                         
+	printf("Size of a float: %f byte(s)", sizeof(float));
 	return (0);
 
 }
