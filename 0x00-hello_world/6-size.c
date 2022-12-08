cf00968b70c1b9c664522ae960b09811255b486b
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2022-12-08 10:22:27 +0100

    Updated 6-size.c
    Changed answer back to %i instead of %1c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %1d byte(s)", sizeof(char));
-	printf("Size of an int: %1d byte(s)", sizeof(int));
-	printf("Size of a long int: %1d byte(s)", sizeof(long int));
-	printf("Size of a long long int: %1d byte(s)", sizeof(long long int));
-	printf("Size of a float: %1d byte(s)", sizeof(float));
+	printf("Size of a char: %i byte(s)", sizeof(char));
+	printf("Size of an int: %i byte(s)", sizeof(int));
+	printf("Size of a long int: %i byte(s)", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)", sizeof(long long int));
+	printf("Size of a float: %i byte(s)", sizeof(float));
 
 	return (0);
 
