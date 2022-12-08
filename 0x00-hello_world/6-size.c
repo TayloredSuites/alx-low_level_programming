5d7df47f424179fb68779e5d252231b4456e40d0
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2022-12-08 11:35:47 +0100

    Updated 6-size.c
    Reduced line chars from 107 to 80

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
 /*
  * main - Entry Point
  *
- * Description : 'Program that prints the size of various types on the computer it is compiled and run on'
+ * Description : 'Program that prints the size of various types on the
+ * computer it is compiled and run on'
  *
  * Return : Always 0 (Success)
  *
