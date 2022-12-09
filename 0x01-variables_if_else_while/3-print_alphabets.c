53e1f356528f83287b57b741b1798fa7af3c9073
TayloredSuites <ibnnureevsoraya@gmail.com>
2022-12-09 01:47:45 -0800

    Added 3-print_alphabets.c for capital and small letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; 1++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z'; 1++)
+	{
+		putchar(i\n);
+	}
+
+	return (0);
