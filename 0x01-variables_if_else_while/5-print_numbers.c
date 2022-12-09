51233acd54e65253a4ccd30edc03ac45ef1a14fb
TayloredSuites <ibnnureevsoraya@gmail.com>
2022-12-09 02:03:39 -0800

    Added 5-print_numbers.c to print single digit numbers only

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * main - Entry Point
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	
+	return (0);
+}
