12834e2393bd7a10a1be2244374b4335b4349e08
TayloredSuites <ibnnureevsoraya@gmail.com>
2022-12-19 00:17:43 -0800

    Added 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int_strlen - a function that returns the length of a  string
+ *
+ * @s: string input
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	char s;
+
+	strlen("%s", s);
+	return (strlen);
+}
