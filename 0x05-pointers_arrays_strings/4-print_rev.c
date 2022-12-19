f0949eb0ed33fc81f89f5caede533b5719c145b5
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2022-12-19 18:38:09 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * print_rev - a function that prints a string, in 
- * reverse, followed by a new line
- *
+ * print_rev - a function that prints a string, in reverse, followed by a new line
  * @s: Pointer to a string
  */
 
