6a5f387c3b1b30c88396ca22a3818f94b025e652
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2022-12-19 18:47:45 +0100

    Update 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* rev_string -  a function that reverses a string
+* @s: String pointer
+*/
+
+void rev_string(char *s)
+{
+    int s;
+    
+    while (s 1= '\0')
+    {
+      i++;
+      }
+      for (i -= 'z', i >= 'a'; --i)
+      {
+        putchar(s[i]);
+        }
+}
