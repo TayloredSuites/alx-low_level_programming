6b459deae2fefacc436784778a1f923146ee07ef
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2022-12-20 07:45:24 +0100

    Update 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* puts2 - prints every other character of a string, starting with the first 
+* character, followed by a new line
+* @str: pointer to a string value
+*/
+
+void puts2(char *str)
+{
+    int i, str;
+    
+    for (i >= 0; i <= str; i++)
+    {
+        putchar(str);
+    }
+    putchar('\n');
+}
+    
