fee7c1b4a3daca847e2ba60e2167516a435f4313
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2022-12-20 11:56:57 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,7 +4,7 @@
  * rev_string - a function that prints a string, in reverse,
  * followed by a new line
  * @s: Pointer to a string
- * Return: length of string as len
+ * Return: length of string as i
  */
 
 void _strlen(char *s)
