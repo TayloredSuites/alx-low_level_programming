88f26f7d87dd0155f94d17729d1f47f647a0e454
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2022-12-20 11:56:17 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,13 +7,20 @@
  * Return: length of string as len
  */
 
+void _strlen(char *s)
+{
+	int i;
+	
+	while (s[i] != '\0')
+		++i;
+	return i;
+}		
+
 void rev_string(char *s)
 {
 	int i;
-	char len;
-	
-	while (s[i] != '\0')
-		i++;
+	char len = _strlen(s);
+
 	for (i = len - 1; i >= 0; i--)
 		putchar(s[i]);
 }
