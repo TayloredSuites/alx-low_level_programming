64f151143941d6e1d207d9a9ed5ef7facb60c44b
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2022-12-21 12:33:32 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,17 +2,16 @@
 
 /**
  * *_strcat - a function that concatenates two strings
- *
+ * @dest: pointer to the initial string
+ * @src: pointer to the string that is added (catenated) to the initial string
  * Return: dest
- *
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	i = 0;
-	j = 0;
+	i = j = 0;
 	while (dest[i] != '\0')
 		i++;
 
