3605de609d40ee6b5866e5a3edcf5f57bbf06555
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2022-12-21 12:38:45 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,27 @@
-#include "main.h"
+# include "main.h"
+
+/**
+ * *_strcat - a function that concatenates two strings
+ * @dest: pointer to the initial string
+ * @src: pointer to the string that is added (catenated) to the initial string
+ * @n: an integer indicating the number of bytes allowed in the src object
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while ((dest[i++] = src[j++] + n) != '\0')
+	{
+		j++;
+		i++;
+	}
+	return (dest);
+}
