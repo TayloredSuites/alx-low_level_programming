91097dd1928deeeb80879bd3c6a362cb96b24931
TayloredSuites <ibnnureevsoraya@gmail.com>
2022-12-22 02:21:13 -0800

    Added 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* *_strncpy -  a function that copies a string
+* _strncpy -  a function that copies a string
 * @dest: pointer to base string being concatenated on
 * @src: string beng catenated with a limit to its
-* @n: max no of string chars to concatenate  of src to dest
-* Pad wit EOF char if src has fewer than n characters.
+* @n: max no of string chars to concatenate  of src to dest, Pad wit EOF char if src has fewer than n characters
+* Return: Always 0 (Success)
 */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -13,9 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	int j;
 	
-	for (j < n; i = 0; (dest[i] = src[i]) != '\0')
+	i = 0;
+	for (j < n;(dest[i] = src[i]) != '\0'; i++)
 	{
 		i++;
 		j++;
 	}
+	return (0)
 }
