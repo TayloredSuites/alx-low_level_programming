e12e595d0a7291cf9f457381f95f06583a787378
TayloredSuites <ibnnureevsoraya@gmail.com>
2022-12-22 12:58:01 -0800

    Added this

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,10 +3,32 @@
 /**
  * cap_string - a function that capitalizes all words of a string
  * @s: string under consideration
+ * Return: all caps string under consideration
  */
 
 char *cap_string(char *)
 {
 	int i, j;
 
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+			{
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) = *(s + i) - 32;
+					}
+				}
+			}
+		}
+	}
 }
