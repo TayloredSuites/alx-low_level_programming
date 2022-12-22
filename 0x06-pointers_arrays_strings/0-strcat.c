013a22f1f1142a538970e8218fb8b6044a66a781
TayloredSuites <ibnnureevsoraya@gmail.com>
2022-12-22 01:23:56 -0800

    Returned return

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,6 +4,7 @@
  * *_strcat - a function that concatenates two strings
  * @dest: pointer to the initial string
  * @src: pointer to the string that is added (catenated) to the initial string
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
@@ -12,8 +13,13 @@ char *_strcat(char *dest, char *src)
 
 	i = j = 0;
 	while (dest[i] != '\0')
+	{
 		i++;
-
-	while ((dest[i++] = src[j++]) != '\0')
-		;
+	}
+	while ((dest[i] = src[j]) != '\0')
+	{
+		i++;
+		j++;
+	}
+	return (dest);
 }
