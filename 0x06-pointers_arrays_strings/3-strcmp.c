018bebe7c03cee6455655f8a2a66bc7314f5bdc5
TayloredSuites <ibnnureevsoraya@gmail.com>
2022-12-22 02:09:47 -0800

    Adjusted linting

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,27 +11,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (; *s1 == *s2; s1++; s2++)
+	for (i = 0; s1[i] == s2[i]; i++)
+	{
 		if (*s1 == '\0')
-			return 0;
-	return *s1 - *s2;
+		{
+			return (0);
+		}
+	}
+	return (s1[i] - s2[i]);
 }
-=======
-#include "main.h"
-
-/**                                                                                                                  
- * _strcmp -  a function that compares two strings returns <0 if s1 < s2                                             
- * @s1: the string which the other is compared against                                                               
- * @s2: this string is compared to the first argument                                                                
- */                                                                                                                  
-                                                                                                                     
-int _strcmp(char *s1, char *s2)                                                                                      
-{                                                                                                                    
-        int i;                                                                                                       
-                                                                                                                     
-        for (; *s1 == *s2; s1++; s2++)                                                                               
-                if (*s1 == '\0')                                                                                     
-                        return 0;                                                                                    
-        return *s1 - *s2;
-}
->>>>>>> 056783b6b1fbc263f79c3c168fe4e866cb6bc6d6
