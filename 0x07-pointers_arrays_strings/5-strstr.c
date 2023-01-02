380a88657dded040e78193a1e7c04a2faf251489
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-02 00:59:19 -0800

    Added mini grep function and updatad main

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - a function that locates a substring
+ * @haystack:string under consideration
+ * @needle:the first occurrence of the substring needle
+ * Return: pointer to the beginning of the located substring, or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+}
