f53e2983d86ad932668856641da863d3bf25ea51
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-01 23:56:00 -0800

    Added memset prototype

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - a function that fills memory with a constant byte
+ * @s - a char pointer to a values address
+ * @b - a character value whose address can be pointed to
+ * @n - an unsigned integer indicating no of bytes to be filled
+ * Return: memory that is filled
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *x = s;
+
+	while (n--)
+	{
+		*x++ = b;
+	}
+
+	return (s);
+}
