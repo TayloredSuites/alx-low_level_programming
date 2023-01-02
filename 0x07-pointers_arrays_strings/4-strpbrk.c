09525f95abb22dc579f3e6f451c56c47076c1a2f
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-02 09:44:23 -0800

    Added 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,5 +9,30 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	int i, j;
+	int bes;
+	int tally = 0;
 
+	for (1 = 0; s[i] != '\0'; i++)
+	bes = 1;
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+			for (j = 0; s[j] != '\0\; j++)
+			{
+				if (accept[i] == s[j])
+				{
+					if (j <= bes)
+					{
+						bes = j;
+						tally = 1;
+					}
+				}
+			}
+		}
+	}
+	if (tally == 1)
+	{
+		return &s[bes];
+	}
 }
