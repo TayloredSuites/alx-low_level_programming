aa17d0a6a16c15e87b09775a91d0e0a8d5cc895d
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-02 00:44:20 -0800

    Added the count string characters

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: a string
+ * @c: character
+ * Return: a character from the string
+ */
+
+char *_strchr(char *s, char c)
+{
+
+}
