549725e058730a609e529b9432c4386fcf1f2ed5
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2023-01-03 03:50:38 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -18,7 +18,7 @@ char *_strpbrk(char *s, char *accept)
 	{
 		for (i = 0; accept[i] != '\0'; i++)
 		{
-			for (j = 0; s[j] != '\0\; j++)
+			for (j = 0; s[j] != '\0'; j++)
 			{
 				if (accept[i] == s[j])
 				{
