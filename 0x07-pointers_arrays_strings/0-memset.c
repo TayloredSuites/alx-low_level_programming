8d3ebff59edb03619bd9ca07b7ed68b9bf20fe6d
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2023-01-03 08:29:30 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -18,3 +18,26 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
+#include <unistd.h>
+
+/**
+
+ * _putchar - writes the character c to stdout
+
+ * @c: The character to print
+
+ *
+
+ * Return: On success 1.
+
+ * On error, -1 is returned, and errno is set appropriately.
+
+ */
+
+int _putchar(char s)
+
+{
+
+	return (write(1, &c, 1));
+
+}
