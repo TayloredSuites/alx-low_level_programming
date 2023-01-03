ea949eaa6d997ac7976aa99872314951183b862a
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2023-01-03 03:54:07 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,7 +4,29 @@
  * print_chessboard - a function that prints the chessboard
  */
 
-oid print_chessboard(char (*a)[8])
+void print_chessboard(char (*a)[8])
 {
+	int i, j;
 
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (j == 7)
+			{
+				_putchar(a[i][j]);
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(a[i][j]);
+			}
+		}
+	}
 }
+
+
+
+
+
+
