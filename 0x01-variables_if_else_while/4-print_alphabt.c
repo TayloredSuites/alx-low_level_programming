748e458cbd96d0e31229241cb96f991479d8e7c5
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 01:52:48 -0800

    Exclude e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,8 +12,11 @@ int main(void)
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar(i);
-		putchar('\n');
+		while (i != 'e' || i != 'q')
+		{
+			putchar(i);
+			putchar('\n');
+		}
 	}
 	return (0);
 }
