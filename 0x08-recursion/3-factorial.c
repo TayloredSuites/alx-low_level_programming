357c9f1b30022c407df6e452ecfcf9add9ad299c
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 10:21:50 -0800

    Added factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - a recursive decrement multplication
+ * @n: number to be factorialed
+ * Return: the factorial to be recursed
+ */
+
+int factorial(int n)
+{
+	if (n <= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
