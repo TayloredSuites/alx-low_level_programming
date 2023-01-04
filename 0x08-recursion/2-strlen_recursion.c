99236de0bc7b1653e84d341c9e05858bb63a93bc
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 10:01:22 -0800

    int _strlen_recursion(char *s)

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string under consideration
+ * Return: length of string under consideration
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len;
+	int i;
+
+	len = 0;
+	for (i = 0; i <= len; i++)
+	{
+		if (*s)
+		{
+			s++;
+			len++;
+		}
+	}
+	return (len);
+
