df51676555361652f5e2fa4c54e733b1b64c7795
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 03:26:47 -0800

    Added 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,20 @@
 #include <Stdio.h>
 
-/*
+/**
  * main - Entry Point
- *
  * Return: Always 0 (Success)
- *
+ * Description: print numbers in hexadecimal
  */
 
 int main(void)
 {
 	int i;
 
-	for ()
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar();
-	}
 
+		putchar("%x", i);
+	}
+	putchar('\n');
 	return (0);
 }
