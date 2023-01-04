f604ddce34c368a34e030780683917765747c32c
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 10:35:41 -0800

    Testing pwer recurson

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - the value of x raised to the power of y
+ * @x: coefficient
+ * @y: power raised to
+ * Return: value of x raised to power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y -1));
+	}
+}
