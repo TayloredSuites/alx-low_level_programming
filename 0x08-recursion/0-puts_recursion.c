eeed7b3df11150639f444d84742848c4b4774e1d
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 05:55:12 -0800

    Added recursion again

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,6 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-if (*s != '\0')
-return s + _puts_recursion(s + 1);
+	if (s* == '\0')
+	{
+		_putchar('\n');
+	}
+	else if (s* != '\0')
+	{
+		_putchar(s);
+		_puts_recursion(s + 1);
+	}
 }
