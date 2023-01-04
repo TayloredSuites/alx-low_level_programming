2cca1f928c2c0d48a31d284acf1ec7279d2d43fd
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 02:26:18 -0800

    Removed trailing whitespace

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry Point
- *
- *
  * Return: Always 0 (Success)
- *
+ * Description: print all base 10 nos less than 10
  */
 
 int main(void)
@@ -16,6 +14,6 @@ int main(void)
 	{
 		printf("%d\n", i);
 	}
-	
+putchar('\n')	
 	return (0);
 }
