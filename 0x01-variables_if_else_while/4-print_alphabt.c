d29dca6d20adc7290f187ad96c8bf05caf419c77
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 02:04:34 -0800

    Added

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,9 +12,10 @@ int main(void)
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar(i);
-		if (i != 'e' || i != 'q')
-		;
+		while (i != 'e' || i != 'q')
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
