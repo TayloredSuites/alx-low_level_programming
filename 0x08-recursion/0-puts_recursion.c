ffa287b43dc3a616fc97d2e9c439f2d7787f133d
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 05:14:08 -0800

    Added void _puts_recursion(char *s)

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a strng followed by a new line
+ * @s: iterative string under consideration
+ * Return: Success (0)
+ */
+
+void _puts_recursion(char *s)
+{
+	
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+New project on recursion using factorials as the tutorial materal.
+
+To run on Ubuntu 20.1
