5544d13bc4510eec7d3dfe88143fec934c8c8576
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 10:47:07 -0800

    5. Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion -  the natural square root of a number
+ * @n: number under condideraton
+ * Return: recursive square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * _sqrt_recursion(n -1));
+	}
+}
