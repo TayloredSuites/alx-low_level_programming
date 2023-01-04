96d68f3336261a3a9d9038714b691aa9b3965df2
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 11:07:19 -0800

    Switched from if to while

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_prime_number - if the input integer is a prime number, otherwise return 0
+ * @n: input integer
+ * Return: 1 if prime number or 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	while (((n % 2) != 0) && (n / n = 1) && (n / 1 = n))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
