3194bd9504420c4dc5870d67fdabc759d365b1c2
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 09:35:50 -0800

    Added reverse string code

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints str in reverse
+ * @s: string under consderation
+ * Return: Always 0 (Success)
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i;
+	int len;
+	char c;
+
+	len = 0;
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		c = s[i];
+		s[i] = s[len - i - 1];
+		s[len - c - 1] = c;
+	}
+	return (_print_rev_recursion(c)));
+}
