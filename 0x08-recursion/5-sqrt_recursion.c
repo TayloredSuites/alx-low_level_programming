13a9e9d89dd987b8e55f888b7feced7eeab6c2e7
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-05 07:02:53 -0800

    Switched main fun logic

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -30,13 +30,13 @@ int _sqrt_recursion(int n)
 {
 	int i = 0;
 
-	if (n == 1)
-	{
-		return (1);
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (1);
 	}
 	else
 	{
