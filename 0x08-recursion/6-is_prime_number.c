f0ea6c663f901cb52d8bde01b363ea59de245566
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-04 16:46:05 -0800

    Added a separate fun

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,37 +1,38 @@
 #include "main.h"
 
 /**
- * is_prime_int - if the input integer is a prime number, otherwise return 0
- * @n: input integer
+ * is_prime_int - if the input integer is a prime number
+ * @i: denumerator
+ * @j: numerator
  * Return: 1 if prime number or 0 if not
  */
 
-int is_prime_int(int n)
+int is_prime_int(int i, int j)
 {
-       if ((n % 2) != 0)
-       {
-	      return (0);
-       }
-      while (((n / n) = 1) && ((n / 1) = n))
+	if ((j % 2) == 0 || ((j / i) != 1))
 	{
-		return (1);
+		return (0);
+	}
+	else if ((i / j) != i)
+	{
+		return (0);
 	}
 	else
 	{
-		return (0);
+		return (is_orime_int(i + 1, j));
 	}
 }
 
 /**
- * is_prime_number - if the input integer is a prime number, otherwise return 0                  
- * @n: input integer                   
- * Return: 1 if prime number or 0 if not
+ * is_prime_number - if the input integer is a prime number
+ * @n: input integer
+ * Return: 1 if prime number or 0 if no
  */
 
 int is_prime_number(int n)
 {
-	if (n > 0)
+	if (n > 1)
 	{
-		return (is_prime_int(n - 1));
+		return (is_prime_int(2, n));
 	}
 }
