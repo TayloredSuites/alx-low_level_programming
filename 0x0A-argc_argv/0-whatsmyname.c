383cf3671d26e084c5d45a0142353b25ddcaaaff
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-06 09:20:39 -0800

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: name of prog and no of cmd lines passed
+ * @argv: contains args used in prog
+ * Return: Always 0 (Success)
+ * Description: prints its name, followed by a new line.
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	if (argc[0])
+	{
+		printf("%s\n", argv[argc]);
+	}
+	return (0)
+}
