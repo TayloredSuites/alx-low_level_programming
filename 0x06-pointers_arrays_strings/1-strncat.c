23ede4218fa8db3b23e0e88f7f6d129db860bb9d
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-06 07:31:53 -0800

    Using while

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,14 +12,20 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = j = 0; j < n && dest[i] != '\0'; i++)
+	i = j = 0;
+	while (*(dest + i))
 	{
-		continue;
+		i++;
 	}
-	for (j = 0; j < n && src[j] != '\0'; j++)
+	while (j < n && *(src +j))
 	{
-		dest[i + j] = src[j];
+		*(dest + i) = *(src +j);
+		i++;
+		j++;
 	}
-	dest[i + j] = '\0';
+	if (j < n)
+	{
+		*(dest + i) = *(src + j);
+	}
 	return (dest);
 }
