32557b7a1d0173de8f8260878ec431b1d525b297
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-06 03:59:52 -0800

    Added int _isupper(int c)

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: chars under consideraton
+ * Return: 1 if true and 0 if lower
+ */
+
+int _isupper(int c)
+{
+	int i = 0;
+
+	if (c)
+	{
+		for (i = 'A'; i <= 'Z'; i++)
+		{
+			return (1);
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
