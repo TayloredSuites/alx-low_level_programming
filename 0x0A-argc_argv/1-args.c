16cafcba35b18992c8758d789fe912a74fcdefa2
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-06 09:53:04 -0800

    Added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints no of args passed 
+ * @argc: no of cmds and/or name of prog
+ * @argv: an array of argc
+ * Return: exit(0)
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	print("%d\n", argc - 1);
+	return (0);
+}
