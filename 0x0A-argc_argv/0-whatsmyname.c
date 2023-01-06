ea1317dd4faa062b813faaba560292a933f229de
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-06 09:42:51 -0800

    Trial and errpr

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
- * @argc: name of prog and no of cmd lines passed
+ * @argc: name of prog and/or array of cmd lines passed
  * @argv: contains args used in prog
  * Return: Always 0 (Success)
  * Description: prints its name, followed by a new line.
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0)
