edee698681c8aa0f16c56d7f4714a533da5d203a
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-08 05:40:30 -0800

    Added this

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,13 +8,8 @@
  * Return: exit(0)
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
-
-	for (i = 1; i <= argc; i++)
-	{
-		printf("Iterating at %d\n", i, argv[i]);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }
