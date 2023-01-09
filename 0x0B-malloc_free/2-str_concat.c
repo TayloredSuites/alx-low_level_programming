e1b39625ee5b246ed2d2ba4e1a3864dd00e4e217
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-09 08:19:13 -0800

    Malloc got slightly easer

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - a function that concatenates two strings memloc
+ * @s1: first string unders consideration
+ * @s2: second string under consideration
+ * Return: pointer and NULL on failure, null terminated concat
+ * Description: if NULL is passed, treat it as an empty string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concated;
+	unsigned int i;
+	unsigned int length;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return ("");
+	}
+	concated = malloc(sizeof(char) * i);
+	if (concated == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= length; i++)
+	{
+		concated[i++] = s[i];
+	}
+	for (i = 0; i <= length; i++)
+	{
+		concated[i++] = s[i];
+	}
+	return (concated);
+}
