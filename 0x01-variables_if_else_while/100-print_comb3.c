fc36624c8f5641e40d77f8b837e7c8ada18a45f4
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-11 06:53:08 -0800

    Attempting

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,16 @@
 
 int main(void)
 {
-	int i;
+	int i, j;
 
-	for (i)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
+		for (j != i; j < 10; j++)
+		{
+			putchar(i);
+			putchar(",");
+		}
 	}
-
+	putchar('\n');
 	return (0);
 }
