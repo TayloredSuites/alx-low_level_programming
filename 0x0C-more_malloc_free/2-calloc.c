5604623a5c3aa5d6a8d553c223efbb06d559b959
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-13 10:37:12 -0800

    In for made it nmemb

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -24,7 +24,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 	/* increment for the pointer value */
-	for (i = 0; i < size; i++)
+	for (i = 0; i < nmemb; i++)
 	{
 		ptr[i] = 0;
 	}
