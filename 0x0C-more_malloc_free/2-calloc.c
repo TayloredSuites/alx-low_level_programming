38aa699ffee125404dfa33d04298975ba94e43b1
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-13 10:39:18 -0800

    Made it sze of alloc i for ptr

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -24,7 +24,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 	/* increment for the pointer value */
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		ptr[i] = 0;
 	}
