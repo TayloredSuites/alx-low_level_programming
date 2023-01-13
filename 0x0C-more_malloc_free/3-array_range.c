6f4c3845f899f8a03c132cf2eb367300a986cf05
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-13 10:34:04 -0800

    Shiffled increment to for loop

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -23,10 +23,9 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	for (i = min; i <= max; i++)
+	for (i = min; i <= max; i++, min++)
 	{
 		ptrarray[i] = min;
-		min++;
 	}
 	return (ptrarray);
 }
