09e7dbfb8a1c4ecef8c136827a37d8c20ccca61d
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-14 00:18:32 -0800

    Added new from stextbook

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,17 @@ void times_table(void)
 {
 	unsigned int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0',);
+	}
+	for (i = 0; i < 9; i++)
+	{
+		_putchar('0,');
+		_putchar('\n');
+		for (j = 0; j < 9; j++)
 		{
-			i++ * j;
-			j++ * i;
+			_putchar(*(i++ * 9 + j));
 		}
 	}
 }
