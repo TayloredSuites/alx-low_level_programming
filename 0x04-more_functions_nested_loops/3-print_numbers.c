e79b2a0f41ff5d098e634c200c0441900a7336d2
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-14 02:23:36 -0800

    Added void print_numbers(void)

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints nums from 0 to 9 and newline
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
