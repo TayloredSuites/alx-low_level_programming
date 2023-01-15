b6f5f875fac4cd2fed651c1243ea653cbbbf502f
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-14 22:40:58 -0800

    Added else statement

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,13 +13,16 @@ void print_square(int size)
 	{
 		_putchar('\n');
 	}
-	for (i = 0; i < size; i++)
+	else
 	{
-		_putchar('#');
-		for (j = 0; j < size; j++)
+		for (i = 0; i < size; i++)
 		{
 			_putchar('#');
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
