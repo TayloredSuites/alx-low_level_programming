1023657645be756cd0e5d6013f70d9f38fe9a7d6
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-14 22:02:26 -0800

    Added void print_line(int n)

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight line in the terminal
+ * @n: length of line specified by user
+ */
+
+void print_line(int n)
+{
+	int i;
+	/* Incrementing for any n scanf("%i", &n) */
+
+	for (i = 0; i < (n + 1); i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
