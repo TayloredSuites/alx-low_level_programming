c21ea68398e9122bc5f72fe7e6841c597894a683
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-17 08:56:25 -0800

    Added 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - a function that frees dogs.
+ * @d: pointer to the structure d of dog_t struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	free(d);
+}
