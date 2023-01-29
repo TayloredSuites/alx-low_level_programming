4598697c977ff5577b435111d8e18479dfa72de5
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-29 04:57:57 -0800

    Made temp pointer

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,13 @@
 size_t print_list(const list_t *h)
 {
 	size_t num_nodes;
-	list_t temp;
+	list_t *temp;
 
 	/* h = (struct node*)malloc(sizeof(struct node)); */
+
 	temp = h;
 	num_nodes = 0;
-	while (temp->str == NULL)
+	if (temp->str == NULL)
 	{
 		printf("[0] (nil)\n");
 	}
