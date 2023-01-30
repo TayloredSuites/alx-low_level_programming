6c85a133d94cc61c2a0e665f777e33a8fc34dbb0
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-30 07:15:36 -0800

    Added file

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_node_end - function that adds a new node at end of list_t list
+ * @head: pointer to the pointer of the head node of the list
+ * @str: the strin gunder consideration for duplication
+ *
+ * Return: address of the new element or NULL if failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *inserted_node;
+	list_t *temp; /* temporary head for traversing */
+
+	inserted_node = (list_t *)malloc(sizeof(list_t));
+	temp = head;
+	/* Traverse to the penultimate node */
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	/* When untrue, make the next temp->next into inserted_node */
+	temp->next = inserted_node;
+	return (inserted_node);
+	free(inserted_node);
+}
