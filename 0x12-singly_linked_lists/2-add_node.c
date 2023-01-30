3174116801b5b32462caa629b6805d32655ffb8a
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-30 05:16:39 -0800

    Gotten structure

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 	
 
-	newnode = (struct list_t*)malloc(sizeof(struct list_t));
+	new_node = (list_t*)malloc(sizeof(list_t));
 	if (new_node != NULL)
 	{
 		new_node->next = *head;
