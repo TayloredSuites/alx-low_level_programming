e6d9f91a3537ce7a615203a7731dcbcc4cd6c368
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-30 08:20:37 -0800

    Added freeing according to QA

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -44,8 +44,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	/* When untrue, make the next temp->next into inserted_node */
 	temp->next = inserted_node;
-	inserted_node->str = strdup(str);
-	inserted_node->len = length;
+	while (inserted_node->str != NULL)
+	{
+		inserted_node->str = strdup(str);
+		inserted_node->len = length;
+	}
 	/* Creating end node termination */
 	inserted_node->next = NULL;
 	return (inserted_node);
