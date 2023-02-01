e003000a46b3da64d41bb5728d709d5edc1d15a6
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-02-01 09:07:53 -0800

    Added 10-delete_nodeint.c again

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,10 +13,17 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *next_node;
 	unsigned int i = 1;
 
+	temp = head;
+	if (head == NULL)
+	{
+		return (-1);
+		free(head);
+	}
 	while (i < (index - 1))
 	{
 		temp = temp->next;
 		i++;
+		return (1);
 	}
 	next_node = temp->next;
 	temp->next = next_node->next;
