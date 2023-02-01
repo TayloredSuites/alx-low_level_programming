84be7831c2a8c451d62c3b63a34cd3e684e94633
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-02-01 09:03:34 -0800

    Add this deletion at position

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index index of a listint_t
+ * @head: pointer to the ponter of th ehead node
+ * @index: position on the list
+ *
+ * Return: Success (1) or failure (-1)
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp;
+	listint_t *next_node;
+	unsigned int i = 1;
+
+	while (i < (index - 1))
+	{
+		temp = temp->next;
+		i++;
+	}
+	next_node = temp->next;
+	temp->next = next_node->next;
+	free(next_node);
+}
