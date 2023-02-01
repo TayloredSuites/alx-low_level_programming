0e244ea8a6c8976599b0b3ade018d1a2c737aa4a
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-01-31 22:51:34 -0800

    Add sad file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list -  a function that frees a list_t list
+ * @head: pointer to the first element of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	/* temp created to leave head pointing to the first element of list */
+	temp = head;
+	/** Let head and temp be equal for base case */
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(temp);
+		head = temp;
+	}
+}
