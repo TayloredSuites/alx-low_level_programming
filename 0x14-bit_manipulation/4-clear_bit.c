b315b69fc299193cb961c3be4b0bc3f04330c4e8
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-02-02 10:01:24 -0800

    Added setter

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * clear_bit - function that sets the value of a bit to 0 at a given index
+ * @n: ponter to the number that s input
+ * @index: position for setting bit
+ *
+ * Return: 1 if it worked and -1 if an error occured
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bit = 0;
+	unsigned long temp = 0 << index;
+
+	if (index > 32)
+	{
+		return (-1);
+	}
+	*n = ((n & ~temp) | bit << index);
+	return (1);
+	return (*n);
+}
