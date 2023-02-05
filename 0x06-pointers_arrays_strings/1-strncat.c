f7c87fd1aa4f6b937bd94471eb077de06af6028e
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-02-05 09:44:52 -0800

    Bety DOS error

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,11 @@
 # include "main.h"
 
 /**
- * *_strncat - a function that concatenates two strings
+ * _strncat - a function that concatenates two strings
  * @dest: pointer to the initial string
  * @src: pointer to the string that is added (catenated) to the initial string
- * @n: an integer indicating the number of bytes allowed in the src object, ie add n characters to src
+ * @n: an integer indicating the number of bytes allowed in the src object, ie
+ * add n characters to src
  * Return: dest
  */
 
@@ -17,9 +18,9 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-	while (j < n && *(src +j))
+	while (j < n && *(src + j))
 	{
-		*(dest + i) = *(src +j);
+		*(dest + i) = *(src + j);
 		i++;
 		j++;
 	}
