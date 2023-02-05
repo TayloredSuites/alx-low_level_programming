7d4c7323096ba7434b09b60d48ba40d4cfcfd78e
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-02-05 08:48:23 -0800

    Added 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,14 +9,11 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int bit = 0;
-	unsigned long int temp = 0 << index;
-
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 	{
 		return (-1);
 	}
-	*n = ((n & ~temp) | bit << index);
+	*n &= ~(1 << index);
 	return (1);
 	return (*n);
 }
