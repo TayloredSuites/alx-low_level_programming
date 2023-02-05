436413aff13ff88895e72d07e6a8a02cd8c9ca01
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-02-05 08:30:10 -0800

    Betty encoding for @

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
 /**
  * get_bit - the value of a bit at a given index
  * @n: number input by user
- * #index: postion of the index
+ * @index: postion of the index
  *
  * Return: value of bit at index or -1 if an error occured
  */
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int value;
 
-	if (index > (sizeof(unsigned long int) * 8 - 1)) /* 8 by 8 is 64 8 */
+	if (index > (sizeof(unsigned long int) * 8 - 1)) /* 8 by 8 is 64 */
 	{
 		return (-1);
 	}
