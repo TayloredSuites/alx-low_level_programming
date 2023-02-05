ebff55edbb41fb76ca41048a3890423b4cccb19a
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-02-05 08:37:26 -0800

    Added correct size of int to bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,10 +12,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned long int bit = 1;
 	unsigned long int temp = 1 << index;
 
-	if (index > 32)
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 	{
 		return (-1);
 	}
 	*n = ((n & ~temp) | bit << index);
-	return (-1);
+	return (1);
 }
