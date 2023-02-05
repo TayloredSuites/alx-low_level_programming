fe4e213f1d656212fe1afc48426dcf6ea831b17b
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-02-05 09:13:29 -0800

    Added 1st file corrections

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,28 +8,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i = 0, remainder, base, exp; /* Increments the loop */
+	int i = 0, remainder; /* Increments the loop */
 	unsigned int converted_num;
-	long double result = 1.0; /* 2 to the power of 0 */
 
 	if (b == NULL)
 	{
 		return (0);
 	}
-	if (b[i] != '0' || b[i] != 1)
+	for (i = 0; b[i]; i++)
 	{
-		return (0);
-	}
-	while (exp != 0)
-	{
-		result *= base;
-		--exp;
-	}
-	for (i = 0; i < _strlen(*b); i++)
-	{
-		remainder = b[i] % 10;
-		b[i] = b[i] / 10;
-		converted_num = converted_num + remainder * result;
+		if (b[i] < '0' || b[i] > '1')
+		{
+			return (0);
+		}
+		converted_num = 2 * converted_num + (b[i] - '0');
 	}
 	return (converted_num);
 }
