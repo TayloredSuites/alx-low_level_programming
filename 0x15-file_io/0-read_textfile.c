b363a898dfcda351ff4aedb08f015fd45d57471c
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-02-06 12:04:18 -0800

    Added 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile -  reads a text file and prints to the POSIX standard output
+ * @filename: pointer to the filename in HD
+ * @letters: no of letters to read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t opener, reader, writer;
+	char *buf;
+
+	if (filename == NULL)
+	{
+		return (NULL);
+	}
+	buf = malloc(sizeof(char) * letters);
+	/* Actual size of mem used */
+	if (buf == NULL)
+	{
+		return (NULL);
+	}
+	opener = open(filename, O_RDONLY);
+	reader = read(opener, buf, letters);
+	writer = write(STDOUT_FILENO, buf, letters);
+	if (opener == -1 || reader == -1 || writer == -1 || reader != writer)
+	{
+		free(buf);
+		close(opener);
+		return (0);
+	}
+	close(opener);
+	free(buf);
+	return (writer);
+}	
