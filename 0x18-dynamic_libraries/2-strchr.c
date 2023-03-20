c55ca4b551ecde2680b427b1ad05aee175e8d389
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-20 04:29:45 -0700

    Added all c files

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: a string
+ * @c: character
+ * Return: a character from the string
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+		{
+			s++;
+		}
+		else
+		{
+			return (s);
+		}
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (0);
+}
