dbddf8bb4b7619791e7417723f5fa9c23a5b3e37
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-20 04:32:25 -0700

    All object and compiler files

---
+++ 0x18-dynamic_libraries/wanted/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp -  a function that compares two strings returns <0 if s1 < s2
+ * @s1: the string which the other is compared against
+ * @s2: this string is compared to the first argument
+ * Return: the difference in the two strings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; i++)
+	{
+		if (s1[i] == '\0')
+		{
+			return (0);
+		}
+	}
+	return (s1[i] - s2[i]);
+}
