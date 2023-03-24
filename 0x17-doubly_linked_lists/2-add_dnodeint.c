a84b69a6746ff9a89951da4932c3a7d4ac1473a7
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-24 03:06:53 -0700

    Removed head NULL case

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,10 +12,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-	if (head == NULL)
-	{
-		return (NULL);
-	}
 	if (new_node == NULL)
 	{
 		return (NULL);
