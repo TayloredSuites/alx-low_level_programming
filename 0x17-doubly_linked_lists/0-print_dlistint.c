571dc5520f571cffd1d82209e3fcdf2041d46ad9
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-24 02:08:14 -0700

    Added temp for traversal

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,17 +9,19 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t num_nodes;
+	const dlistint_t *temp;
 	int n;
 
 	/* h = (struct d_listint_t*)malloc(sizeof(struct dlistint_t)); */
 
 	num_nodes = 0;
-	n = h->n
-	while (h != NULL)
+	temp = h;
+	n = temp->n
+	while (temp != NULL)
 	{
 		num_nodes++;
 		printf("%d\n", n);
-		h = h->next;
+		temp = temp->next;
 		/* increment no nodes then go to next if not NULL */
 	}
 	return (num_nodes);
