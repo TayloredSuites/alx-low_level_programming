ff467defab127211b92951a9a7a93530f632f439
TayloredSuites <ibnnureevsoraya@gmail.com>
2023-03-24 02:19:16 -0700

    Added Return statement

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,7 +4,7 @@
  * print_dlistint - function that prints all the elements of a dlistint_t list
  * @h: pointer to address of head node
  *
- * Return - no of nodes of size_t
+ * Return: no of nodes of size_t
  */
 size_t print_dlistint(const dlistint_t *h)
 {
