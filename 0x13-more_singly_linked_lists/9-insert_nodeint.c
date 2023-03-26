f62251cc24429ae2d503a47d663d3c1921559a5f
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2023-03-26 11:21:22 +0100

    Corrected 'unisghed' spelling

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *new_node;
-	unisgned int length_l;
+	unsigned int length_l;
 	unsigned int i;
 
 	/* Length_l is the length of the linked list */
