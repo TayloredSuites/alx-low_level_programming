31d83c1a9fe5387fc0e9ce045b878c568be2cbf8
Taylored Suites <ibnnureevsoraya@gmail.com>
2023-09-04 05:05:41 -0700

    Updated 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,10 +10,30 @@
 
 int main(int argc, char *argv[])
 {
-	int i = 2, j = 0, arg_val;
+	int i, j = 1, arg_val, sum = 0;
 	char argv;
 
-	arg_val = atoi(argv[i]);
-	if (arg_val > 0)
+	arg_val = atoi(argv[j]);
+	for (i = 2; i = argc - 1; j++)
 	{
-		arg_val += arg_val
+		if (arg_val > 0)
+		{
+			sum += arg_val;
+			printf("%d\n", sum);
+		}
+		if (arg_val == NULL)
+		{
+			printf("0\n");
+		}
+		if (arg_val != int)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	return (0);
+		
