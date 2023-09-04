10bd1c2bda428425c130f60199f2f78fab08bbaa
Taylored Suites <ibnnureevsoraya@gmail.com>
2023-09-04 03:46:52 -0700

    Added 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc)
+	{
+		for (i = 0; i > arg c; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
+	}
+	return 0;
+}
