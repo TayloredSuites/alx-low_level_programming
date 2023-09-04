21ba3856d8a6264931a77b14858593f68643b235
Taylored Suites <ibnnureevsoraya@gmail.com>
2023-09-04 03:22:04 -0700

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the program and newline
+ * @agrc: argument count
+ * @argv: argument vector (pointer to string array)
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return 0;
+}
