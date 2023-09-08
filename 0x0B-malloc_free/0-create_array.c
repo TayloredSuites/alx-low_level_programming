ceed064f5cc68405b75148ec73bf1b448538d66f
Taylored Suites <ibnnureevsoraya@gmail.com>
2023-09-08 02:39:04 -0700

    Relogicked 0-create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,19 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
+	int i; /* For traversal */
 
 	array = malloc((*char) * size);
 	if (size == 0)
 	{
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = 'c';
+	}
+	return (array);
+	free(array);
+	return (0);
 }
 
