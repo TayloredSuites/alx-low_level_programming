deb28567f3d21d07f7e37467c5b19979ac69af0e
Taylored Suites <ibnnureevsoraya@gmail.com>
2023-09-11 02:28:23 -0700

    Added string duplicate file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "amin.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _strdup - Pointer to duplicated string in memory
+ * @str: string to be duplicated
+ *
+ * Return: Duplicated string memory
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	int i;   /* For traversal */
+	int len;   /* length of the string */
+
+	for (len = 0, *str != '\0'; str++)
+	{
+		len++;
+	}
+	dup = malloc(sizeof(char) * len);
+	for (i = 0; i < len; i++)
+	{
+		dup[i] = str[i];
+	}
+	return (dup);
+	free (dup);
+}
