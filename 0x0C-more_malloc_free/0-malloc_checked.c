fa9e1ed33bd21d6bb0e0d3427b833c9dc19fd612
Taylored Suites <ibnnureevsoraya@gmail.com>
2023-09-13 03:35:40 -0700

    Changed ptr

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -12,7 +12,7 @@ void *malloc_checked(unsigned int b)
 {
        void *pointer;
 
-       pointer = malloc(*b);
+       pointer = malloc(b);
        if (pointer == NULL)
        {
 	       return (98);
