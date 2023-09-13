55672a6a6ff2ad9a17539e49e8ae701a53ab807a
Taylored Suites <ibnnureevsoraya@gmail.com>
2023-09-13 03:50:28 -0700

    Changed b to unsigned int

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -12,7 +12,7 @@ void *malloc_checked(unsigned int b)
 {
        unsigned int *points;
 
-       points = malloc(sizeof(b));
+       points = malloc(sizeof(unsigned int));
        if (points == NULL)
        {
 	       exit (98);
