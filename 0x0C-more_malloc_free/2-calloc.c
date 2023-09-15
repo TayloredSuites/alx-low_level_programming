f96f0d3b2c177cd934df1e131fbcd011d91ceb71
Taylored Suites <ibnnureevsoraya@gmail.com>
2023-09-15 04:34:47 -0700

    Added _calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _calloc - Allocates emory for an array using malloc
+ * @nmemb: no of elements in the array
+ * @size: no of bytes per member
+ * Return: if malloc fails return NULL, if nmemb or size is 0
+ * return NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;    /* traversal value */
+	unsigned int *mem_alloc;
+
+	if (nmemb == NULL)
+	{
+		return (NULL);
+	}
+	if (size == NULL)
+	{
+		return (NULL);
+	}
+	mem_alloc = malloc(sizeof(unsigned int) * size);
+	if (mem_alloc == NULL)
+	{
+		return (NULL);
+	}
+	return (mem_alloc);
+}
