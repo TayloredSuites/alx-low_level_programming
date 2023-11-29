00acee7221c93f237ea739fe90b96ca42eeff832
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2023-11-29 10:51:14 +0100

    Added 0x17-doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * dlistint_len -  the number of elements in a linked dlistint_t list
+ * @h: pointer to head node address
+ *
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t elements;
+	const dlistint_t *temp;
+
+	temp = h;
+	elements = 0;
+	while (temp != NULL)
+	{
+		elements++;
+		temp = temp->next;
+		/* increase number of elements by 1 then go to next */
+	}
+	return (elements);
+}
