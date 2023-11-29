c218dbd9cd5198ea8e95cdc8b56322b4fd2225ed
Glory E <105065655+TayloredSuites@users.noreply.github.com>
2023-11-29 10:20:57 +0100

    Added lists.h, list_len, 0-print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_list - a function that prints all the elements of a list_t list
+ * @h: pointer to head node
+ *
+ * Return: the number of nodes of type size_t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num_nodes;
+
+	/* h = (struct node*)malloc(sizeof(struct node)); */
+
+	num_nodes = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			num_nodes++;
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+	/* Increment no of nodes, print the len and str,then move to the next */
+		}
+	}
+	return (num_nodes);
+}
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * _strlen - aa function that returns the length of a string
+ * @s: string under consideration
+ *
+ * Return: Always 0 (Success)
+ */
+int _strlen(const char *s)
+{
+	int len;
+
+	for (len = 0; *s != '\0'; s++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * add_node - a function that adds a new node at the beginning of a list_t list
+ * @head: pointer to the head pointer of the list
+ * @str: string that needs to be duplicated
+ *
+ * Return: address of new element or null if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	int length;
+
+	new_node = (list_t *)malloc(sizeof(list_t));
+	length = _strlen(str);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	new_node->len = length;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+	free(new_node);
+}
